9d1c544fda426ac099db6dbf9458e3fedbf7e7da
Unknown <khamaz.ee@gmail.com>
2018-12-29 18:27:40 +0100

    Added Music

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip music;
+    
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = music;
+        source.loop = true;
+        source.Play();
+    }
+}
