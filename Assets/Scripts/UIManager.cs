fae08bf2974cb9654c1ff955ce71d29c7b92494b
Unknown <khamaz.ee@gmail.com>
2018-12-29 17:59:25 +0100

    UIManager + Canvas

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject toHide;
+
+    public void StartGame()
+    {
+        player.SetActive(true);
+        toHide.SetActive(false);
+    }
+
+    public void Reset()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
